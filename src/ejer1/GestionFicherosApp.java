d1aa656451c1185c3858791872ca7b0257c2aca7
Juanjo Bartual <Juanjo Bartual>
2017-09-18 19:31:19 +0200

    Acceso a datos

---
+++ src/ejer1/GestionFicherosApp.java
@@ -0,0 +1,13 @@
+package ejer1;
+
+import gestionficheros.MainGUI;
+
+public class GestionFicherosApp {
+
+	public static void main(String[] args) {
+		GestionFicherosImpl getFicherosImpl = new GestionFicherosImpl();
+		new MainGUI(getFicherosImpl).setVisible(true);
+
+	}
+
+}
